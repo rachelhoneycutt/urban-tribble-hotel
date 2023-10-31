5e1f9ed0fed97d310d44bd6890962ec80e5fa993
rachelhoneycutt <142539235+rachelhoneycutt@users.noreply.github.com>
2023-10-31 14:20:55 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,27 @@
-$(function() {
-  
-  //click the eye icon w/class reveal 
-  $(".reveal").on('click',function() {
-    let $pwd = $(".pwd");
+$(document).ready(function(){
+    $('.signUp').toggle();  
     
-    if ($pwd.attr('type') === 'password') {
-        $pwd.attr('type', 'text');
-       $(this).children('i').addClass('bi-eye-slash-fill').removeClass('bi-eye-fill');
-    } else {
-        $pwd.attr('type', 'password');
-     $(this).children('i').removeClass('bi-eye-slash-fill').addClass('bi-eye-fill');
-    }
-});
+    $('#signUpBTN').click(function(){
+      $('.loginPage').toggle();
+      $('.signUp').toggle(); 
+    });
+    
+  });
   
-});
+  $(function() {
+    
+    //click the eye icon w/class reveal 
+    $(".reveal").on('click',function() {
+      let $pwd = $("#pwd");
+      
+      if ($pwd.attr('type') === 'password') {
+          $pwd.attr('type', 'text');
+         $(this).children('i').addClass('bi-eye-slash-fill').removeClass('bi-eye-fill');
+      } else {
+          $pwd.attr('type', 'password');
+       $(this).children('i').removeClass('bi-eye-slash-fill').addClass('bi-eye-fill');
+      }
+  });
+    
+  });
+  
