6985733855e6f8c1ac7a5111774302e320ad6b46
rachelhoneycutt <142539235+rachelhoneycutt@users.noreply.github.com>
2023-10-31 13:21:16 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+$(function() {
+  
+  //click the eye icon w/class reveal 
+  $(".reveal").on('click',function() {
+    let $pwd = $(".pwd");
+    
+    if ($pwd.attr('type') === 'password') {
+        $pwd.attr('type', 'text');
+       $(this).children('i').addClass('bi-eye-slash-fill').removeClass('bi-eye-fill');
+    } else {
+        $pwd.attr('type', 'password');
+     $(this).children('i').removeClass('bi-eye-slash-fill').addClass('bi-eye-fill');
+    }
+});
+  
+});
